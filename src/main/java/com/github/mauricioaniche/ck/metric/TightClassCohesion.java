11549768b2ec6ac95d8fd25f661f5ddac24f3f21
Jan Gerling <jan.gerling@posteo.de>
2020-05-11 11:01:26 +0200

    Create indirect method invocation map with calling functions

--- src/main/java/com/github/mauricioaniche/ck/metric/TightClassCohesion.java
+++ src/main/java/com/github/mauricioaniche/ck/metric/TightClassCohesion.java
@@ -2,6 +2,7 @@ package com.github.mauricioaniche.ck.metric;
 
 import com.github.mauricioaniche.ck.CKClassResult;
 import com.github.mauricioaniche.ck.CKMethodResult;
+import com.google.common.collect.Sets;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.util.HashMap;
@@ -13,9 +14,9 @@ Calculates the tight and loose class cohesion for a class.
 For more details see: https://www.aivosto.com/project/help/pm-oo-cohesion.html#TCC_LCC
  */
 public class TightClassCohesion implements CKASTVisitor, ClassLevelMetric {
-    HashMap<String, Set<String>> allAccessedFields = new HashMap<>();
     HashMap<String, Set<String>> accessedFields = new HashMap<>();
 
+    /*
     //Two methods are directly connected if:
     //1. both access the same class-level variable
     //2. their call trees access the same class-level variable (only within the class)
@@ -23,23 +24,30 @@ public class TightClassCohesion implements CKASTVisitor, ClassLevelMetric {
         for (CKMethodResult method : result.getMethods()){
             accessedFields.put(method.getMethodName(), method.getFieldsAccessed());
         }
+
+        HashMap<String, Set<String>> allAccessedFields = new HashMap<>();
         for (CKMethodResult method : result.getMethods()){
-            generateAccessTree(method, new HashSet<>());
+            Set<String> allLocalFields = generateAccessTree(method);
+            allLocalFields.addAll(method.getFieldsAccessed());
+            allAccessedFields.put(method.getMethodName(), allLocalFields);
         }
 
 
-        Set<ImmutablePair> directConnections = new HashSet<>();
         return directConnections;
     }
 
-    private void generateAccessTree(CKMethodResult method, Set<String> explored){
-        //we already explored the call tree, thus we can just use the found invocations
-        if(allAccessedFields.containsKey(method.getMethodName())){
-            return;
+    //Get all accessed fields from the invocation tree of a method
+    //This currently has a worst runtime of N * N!
+    private Set<String> generateAccessTree(CKMethodResult method){
+        Set<String> allLocalInvocations = Sets.newHashSet(method.getMethodInvocationsLocal());
+        allLocalInvocations.addAll(method.getMethodInvocationsIndirectLocal());
+
+        Set<String> allLocalFields = new HashSet<>();
+        for (String invocation : allLocalInvocations){
+            allLocalFields.addAll(accessedFields.get(invocation));
         }
-        for (CKMethodResult nextMethod : method.()){
-            accessedFields.put(method.getMethodName(), method.getFieldsAccessed());
-        }
+
+        return allLocalFields;
     }
 
     //Two methods are indirectly connected if:
@@ -49,12 +57,12 @@ public class TightClassCohesion implements CKASTVisitor, ClassLevelMetric {
         Set<ImmutablePair> indirectConnections = new HashSet<>();
 
         return indirectConnections;
-    }
+    }*/
 
     public void setResult(CKClassResult result) {
         //maximum number of possible connections (N * (N -1))
         float np = result.getMethods().size() * (result.getMethods().size() -1);
-
+        /*
         //number of direct connections (number of edges in the connection graph) in this class
         float directConnections = getDirectConnections(result).size();
         //number of indirect connections in this class
@@ -62,5 +70,6 @@ public class TightClassCohesion implements CKASTVisitor, ClassLevelMetric {
 
         result.setTightClassCohesion(directConnections / np);
         result.setLooseClassCohesion((directConnections + indirectConnections) / np);
+        */
     }
 }
